d52d47bb1d84ec8c7dca58498f038c4c916fedff
Erika Sánchez-Femat <erikasafe@gmail.com>
2023-01-27 21:28:50 -0600

    [FIX] project path and metadata information [Author] Erika Sánchez Femat
    -- Fixed the project path that was causing trouble to find the artifacts dir
    -- Deleted the version key of the metadata json file

--- app/services/load_model.py
+++ app/services/load_model.py
@@ -3,9 +3,11 @@ import pickle
 from loguru import logger
 
 
-def _load_local_model(path: str):
+def load_local_model(path: str):
     try:
         model = pickle.load(open(path, "rb"))
         logger.info("Model loaded successfully")
     except Exception as e:
         logger.error(f"Error loading model {e}")
+    
+    return model
